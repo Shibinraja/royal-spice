5b942a7cb9a639d20296ceb22e9e755470093b0b
Shibin Raja <shibinraja@engineer.com>
2025-04-11 04:31:45 +0530

    fix: firebase updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const NextConfig = {
+  reactStrictMode: true,
+  // This line is important for Firebase integration
+  output: "export",
+  // Only add this if you don't have images that use Next.js Image optimization
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = NextConfig;
